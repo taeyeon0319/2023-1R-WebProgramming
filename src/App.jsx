c556c9a9d1ea47bbb7bd85ff95bfe8e353025cbd
taeyeon0319 <xodus1349@likelion.org>
2023-05-07 23:51:03 +0900

    9주차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [row, setRow] = useState([]);
+  if (row.length == 0) {
+    const res = fetch("http://openapi.seoul.go.kr:8088/78636e4e496269673634556a795559/json/RealtimeCityAir/1/25").then(
+      function (res2) {
+        res2.json().then(function (res3) {
+          setRow(res3.RealtimeCityAir.row);
+        })
+      }
+    )
+  }
+  console.log(row);
+
 
   return (
     <>
@@ -17,14 +28,28 @@ function App() {
         </a>
       </div>
       <h1>Vite + Resoto</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      {/* 여기부터 */}
+      <table>
+        <thead>
+          <th>이름</th>
+          <th>PM10</th>
+          <th>O3</th>
+          <th>상태</th>
+        </thead>
+        <tbody>
+          {
+            row.map(function (obj) {
+              return <tr>
+                <td>{obj.MSRSTE_NM}</td>
+                <td>{obj.PM10}</td>
+                <td>{obj.O3}</td>
+                <td>{obj.IDEX_NM}</td>
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more울랄라~~
       </p>
